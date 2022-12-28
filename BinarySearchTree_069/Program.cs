3fa7dc7db70f3fa7e40e16b7b509ee9140141ce5
TA1069 <maylafaat@gmail.com>
2022-12-28 17:09:45 +0700

    add class node

--- BinarySearchTree_069/Program.cs
+++ BinarySearchTree_069/Program.cs
@@ -2,8 +2,19 @@
 
 namespace BinarySearchTree_069
 {
-    class Program
-    {
+        class Node
+        {
+            public string info;
+            public string leftchild;
+            public string rightchild;
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
