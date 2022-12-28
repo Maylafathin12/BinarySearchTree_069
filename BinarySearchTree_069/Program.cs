f0b82cce6a33b38dba58523584aef0b723a0c194
TA1069 <maylafaat@gmail.com>
2022-12-28 17:14:28 +0700

    add class program

--- BinarySearchTree_069/Program.cs
+++ BinarySearchTree_069/Program.cs
@@ -2,22 +2,32 @@
 
 namespace BinarySearchTree_069
 {
-        class Node
-        {
-            public string info;
-            public string leftchild;
-            public string rightchild;
-
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+        //constructor for the Node Class
         public Node(string i, Node l, Node r)
         {
             info = i;
             leftchild = l;
             rightchild = r;
         }
+    }
+    //a node class consist of three things, the information, revfernces to the right child, and
+    //references to the left child.
+
+    class program
+    {
+        public Node ROOT;
+        public program()
+        {
+            ROOT = null; /* Intializing ROOT to null*/
         }
+    }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
-}
